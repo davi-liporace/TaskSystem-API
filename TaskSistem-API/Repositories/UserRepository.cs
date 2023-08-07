5858f189e2bfd31e0c3de54743c327b1ed2b298f
davi-liporace <davimarquesr@gmail.com>
2023-08-07 14:57:31 -0300

    Created basic repositories

--- TaskSistem-API/Repositories/UserRepository.cs
+++ TaskSistem-API/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using TaskSistem_API.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TaskSistem_API.Data;
 using TaskSistem_API.Models;
 using TaskSistem_API.Repositories.Interfaces;
 
@@ -10,30 +11,54 @@ namespace TaskSistem_API.Repositories
         public UserRepository(TaskSystemDBContext taskSystemDBContext) {
             _dbContext = taskSystemDBContext;
         }
-        public Task<List<UserModel>> GetAll()
+        public async Task<List<UserModel>> GetAll()
         {
-            throw new NotImplementedException();
+            return await _dbContext.Users.ToListAsync();
         }
 
         public async Task<UserModel> GetById(int id)
         {
-            throw new NotImplementedException();
+            return await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == id) ;
         }
 
-        public Task<UserModel> Add(UserModel user)
+        public async Task<UserModel> Add(UserModel user)
         {
-            throw new NotImplementedException();
+            await _dbContext.Users.AddAsync(user);
+            _dbContext.SaveChanges();
+
+            return user;
         }
 
-        public Task<bool> Delete(int id)
+        public async Task<UserModel> Update(UserModel user, int id)
         {
-            throw new NotImplementedException();
+            UserModel userById = await GetById(id);
+            if(userById == null)
+            {
+                throw new Exception($"User for ID:{id} was not found in database");
+
+            }
+            userById.Name = user.Name;
+            userById.Email = user.Email;
+
+            _dbContext.Users.Update(userById);
+            _dbContext.SaveChanges();
+
+            return userById;           
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            UserModel userById = await GetById(id);
+            if (userById == null)
+            {
+                throw new Exception($"User for ID:{id} was not found in database");
+            }
+            _dbContext.Users.Remove(userById);
+            _dbContext.SaveChanges() ;
+            return true;
         }
 
     
-        public Task<UserModel> Update(UserModel user, int id)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
